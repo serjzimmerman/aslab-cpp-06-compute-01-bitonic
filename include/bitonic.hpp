bfba92177c493add643b20f714282bf1b880775b
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-02-13 21:41:02 +0300

    refactore bitonic

--- include/bitonic.hpp
+++ include/bitonic.hpp
@@ -15,6 +15,7 @@
 #include "utils.hpp"
 
 #include <bit>
+#include <chrono>
 #include <memory>
 #include <span>
 #include <stdexcept>
@@ -72,46 +73,76 @@ class naive_bitonic_kernel : public i_bitonic_kernel_source {
   std::string m_source;
 
 public:
-  naive_bitonic_kernel(const std::string type) {
-    static const std::string bitonic_naive =
-        R"(
-    __kernel void naive_bitonic (__global TYPE *in, __global TYPE *out, int step, int stage) {
-      int i = get_global_id(0);
-      int seq_len = 1 << (stage + 1);
-      int power_of_two = 1 << (step - stage);
-      int seq_n = i / seq_len;
-      int odd = seq_n / power_of_two;
-      bool increasing = ((odd % 2) == 0);
-      int halflen = seq_len / 2;
-      if (i < (seq_len * seq_n) + halflen) {
-        int   j = i + halflen;
-        if (((in[i] > in[j]) && increasing) ||
-            ((in[i] < in[j]) && !increasing)) {
-          out[i] = in[j];
-          out[j] = in[i];
+  template <typename T> class gpu_bitonic : public i_bitonic_sort<T>, protected : clutils::platfrom_selector {
+  protected:
+    cl::Context      m_ctx;
+    cl::CommandQueue m_queue;
+
+    static constexpr clutils::platform_version cl_api_version = {2, 2};
+
+    gpu_bitonic()
+        : clutils::platform_selector{cl_api_version}, m_ctx{m_device}, m_queue{m_ctx, cl::QueueProperties::Profiling} {}
+
+    using func_signature = cl::Event(cl::Buffer, int, int);
+
+    void run_boilerplate(std::span<T> container, std::function<func_signature> func, clutils::profiling_info *time) {
+      unsigned size = container.size();
+      if (std::popcount(size) != 1 || size < 2) throw std::runtime_error("Only power-of-two sequences are supported");
+
+      auto wall_start = std::chrono::high_resolution_clock::now();
+
+      int steps_n = std::countr_zero(size);
+      for (int step = 0; step < steps_n; ++step) {
+        int stage_n = step;
+        for (int stage = stage_n; stage >= 0; --stage) {
+          cl::Buffer buff = {m_ctx, CL_MEM_READ_WRITE, clutils::sizeof_container(container)};
+          cl::copy(m_queue, container.begin(), container.end(), buff);
+          auto event = func(buff, step, stage);
+          event.wait();
+          cl::copy(m_queue, buff, container.begin(), container.end());
         }
       }
+      auto                     wall_end = std::chrono::high_resolution_clock::now();
+      std::chrono::nanoseconds pure_start{event.getProfilingInfo<CL_PROFILING_COMMAND_START>()},
+          pure_end{event.getProfilingInfo<CL_PROFILING_COMMAND_END>()};
+      auto pure = std::chrono::duration_cast<std::chrono::milliseconds>(pure_end - pure_start);
+      auto wall = std::chrono::duration_cast<std::chrono::milliseconds>(wall_end - wall_start);
 
-    })";
-  }
-};
+      if (time) *time = {pure, wall};
+    }
+  };
 
-template <typename T> class gpu_bitonic_sort : public i_bitonic_sort<T>, protected clutils::platform_selector {
-private:
-  std::unique_ptr<i_bitonic_kernel_source> m_source;
+  class naive_bitonic : public gpu_bitonic {
+    struct kernel {
+      using functor_type = cl::KernelFunctor<cl::Buffer, int, int>;
+      static std::string source(std::string type) {
+        static const std::string naive_source = R"(
+        __kernel void naive_bitonic (__global TYPE *buff, int step, int stage) {
+          int i = get_global_id(0);
+          int seq_len = 1 << (stage + 1);
+          int power_of_two = 1 << (step - stage);
+          int seq_n = i / seq_len;
+          int odd = seq_n / power_of_two;
+          bool increasing = ((odd % 2) == 0);
+          int halflen = seq_len / 2;
+          if (i < (seq_len * seq_n) + halflen) {
+            int   j = i + halflen;
+            if (((buff[i] > buff[j]) && increasing) ||
+                ((buff[i] < buff[j]) && !increasing)) {
+              TYPE tmp = buff[j];
+              buff[i] = buff[j];
+              buff[j] = tmp;
+            }
+          }
+        })";
 
-  cl::Context      m_ctx;
-  cl::CommandQueue m_queue;
-  cl::Program      m_program;
+        auto type_macro_def = clutils::kernel_define("TYPE", type);
+        return type_macro_def + naive_source;
+      }
 
-public:
-  gpu_bitonic_sort(std::unique_ptr<i_bitonic_kernel_source> &&source)
-      : m_source{std::move(source)}, m_ctx{m_device}, m_queue{m_ctx, cl::QueueProperties::Profiling},
-        m_program{m_ctx, m_source->get_kernel_source, clutils::opencl_build_option::strict} {}
+      static std::string entry() { return "naive"; }
+    };
 
-  void operator()(std::span<T> container) const override {
-    unsigned size = container.size();
-    if (std::popcount(size) != 1 || size < 2) throw std::runtime_error("Only power-of-two sequences are supported");
-  }
-};
-} // namespace bitonic
+  private:
+  };
+}
