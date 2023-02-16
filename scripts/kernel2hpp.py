41b17d43588d72e68efbde0ce87645d8900c31f4
Sergei Zimmerman <serjtsimmerman@gmail.com>
2023-02-16 11:43:48 +0300

    Clean up kernel2hpp

--- scripts/kernel2hpp.py
+++ scripts/kernel2hpp.py
@@ -12,7 +12,7 @@ import re
 import json
 
 
-def main():
+def parse_cmd_args():
     parser = ArgumentParser(
         prog="kernel2hpp",
         description="Convert OpenCL .cl kernels to C++ .hpp headers to embed kernel code directly in your projects")
@@ -23,70 +23,64 @@ def main():
     parser.add_argument("-o", "--output", dest="output",
                         help="output header", metavar="")
 
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    name_without_ext = Path(args.input).with_suffix("").name
+
+def main():
+    args = parse_cmd_args()
+
     with open(args.input) as input:
-        kernel_text = input.read()
+        kernel_source = input.read()
 
-    opts = re.findall(r"@(\w+).*(\[.*\])", kernel_text)
-    optmap = {}
-    for i in opts:
-        optmap[i[0]] = json.loads(i[1])
+    filename_without_ext = Path(args.input).with_suffix("").name
 
-    if "kernel" not in optmap:
-        optmap["kernel"] = str(name_without_ext)
+    pragmas = re.findall(r"@(\w+).*(\[.*\])", kernel_source)
+    pragmap = {}
+    for i in pragmas:
+        pragmap[i[0]] = json.loads(i[1])
+
+    kernel_class_name = str(filename_without_ext) if "kernel" not in pragmap else pragmap[
+        "kernel"][0]
+    output_path = Path(args.output if args.output is not None else "./")
+    macros = {} if "macros" not in pragmap else pragmap["macros"]
+    functor_args = ", ".join(pragmap["signature"])
+
+    if output_path.is_dir():
+        output_file = str(
+            Path(args.output + kernel_class_name).with_suffix(".hpp"))
     else:
-        optmap["kernel"] = optmap["kernel"][0]
-
-    if (args.output is None):
-        args.output = "./"
-
-    oput_path = Path(args.output)
-    if (oput_path.is_dir()):
-        output = str(
-            Path(args.output + optmap["kernel"]).with_suffix(".hpp"))
-    else:
-        output = args.output
+        output_file = args.output
 
     header_text = "#include <CL/opencl.hpp>\n#include <string>\n#include <utils.hpp>\n\n"
-    header_text += "struct {} {{ \n".format(optmap["kernel"])
-
+    header_text += "struct {} {{ \n".format(kernel_class_name)
     header_text += "\tusing functor_type = cl::KernelFunctor<{}>;\n\n".format(
-        ", ".join(optmap["signature"]))
+        functor_args)
+    source_args = ["{} {}_param".format(i["type"], i["name"]) for i in macros]
 
-    source_args = []
-    if "macros" not in optmap:
-        optmap["macros"] = {}
-
-    for i in optmap["macros"]:
-        source_args.append("{} {}_param".format(i["type"], i["name"]))
-
-    header_text += "\tstatic std::string source({}) {{\n\t\tstatic const std::string {}_source = R\"(\n{})\";\n\n".format(", ".join(source_args), optmap["kernel"],
-                                                                                                                          kernel_text)
-    for i in optmap["macros"]:
-        header_text += "\t\tauto {}_macro_def = clutils::kernel_define(\"{}\", {}_param);\n".format(
-            i["name"], i["name"], i["name"])
+    header_text += "\tstatic std::string source({}) {{\n\t\tstatic const std::string {}_source = R\"(\n{})\";\n\n".format(", ".join(source_args), kernel_class_name,
+                                                                                                                          kernel_source)
+    for i in macros:
+        macro_name = i["name"]
+        header_text += "\t\tauto {0}_macro_def = clutils::kernel_define(\"{0}\", {0}_param);\n".format(
+            macro_name)
 
     header_text += "\t\treturn "
 
-    names = []
-    for i in optmap["macros"]:
-        names.append("{}_macro_def".format(i["name"]))
-    names.append("{}_source".format(optmap["kernel"]))
+    names = ["{}_macro_def".format(i["name"]) for i in macros]
+    names.append("{}_source".format(kernel_class_name))
 
     header_text += " + ".join(names) + ";\n"
 
     header_text += "\t}\n\n"
     header_text += "\tstatic std::string entry() {{ return \"{}\"; }}\n".format(
-        optmap["entry"][0])
+        pragmap["entry"][0])
 
     header_text += "};\n"
 
-    with open(output, "w") as oput:
+    with open(output_file, "w") as oput:
         oput.write(header_text)
 
-    print(optmap)
+    print(pragmap)
 
 
 main()
